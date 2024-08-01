5ac32a6bcfe20bf3da113415271e3cc40423abf9
maeve <maevechamberlain27@gmail.com>
2024-08-01 12:48:01 +1000

    updated to complete worksheet 3
    :3

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Monster : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Player Stats
+    public float health = 40;
+    public float maxHealth = 40;
+    public float movementSpeed = 1.0f;
+    public float attacksPerSecond = 1.0f;
+    public float attackRange = 2.0f;
+    public float attackDamage = 10.0f;
+
+    // Cache references to important components for easy access later.
+    private NavMeshAgent agentNavigation;
+    private Animator animator;
+    private Player player;
+
     void Start()
     {
-        
+        agentNavigation = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        player = GameObject.FindObjectOfType<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateMovement();
+    }
+
+    // Handle all update logic associated with the character's movement.
+    private void UpdateMovement()
+    {
+        animator.SetFloat("Speed", agentNavigation.velocity.magnitude);
+        if (Input.GetMouseButton(0))
+            agentNavigation.SetDestination(player.transform.position);
     }
 }
---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns = 2.0f;
+    public float spawnRadius = 10.0f;
+    public Monster monsterToSpawn;
+    public GameObject monsterSpawnEffect;
+    private float nextSpawnAt;
+
+    void Update()
+    {
+        if (monsterToSpawn != null && Time.time > nextSpawnAt)
+        {
+            // Calculate the correct spawn location (given the set spawn radius).
+            Vector3 spawnPosition = transform.position + Random.insideUnitSphere * spawnRadius;
+            spawnPosition.y = transform.position.y;
+
+            // Calculate when the next monster should be spawned.
+            nextSpawnAt = Time.time + timeBetweenSpawns;
+
+            // Spawn the monster at the correct spawn location.
+            Instantiate(monsterToSpawn.gameObject, spawnPosition, transform.rotation);
+
+            // If a spawn effect has been assigned, spawn it.
+            if (monsterSpawnEffect != null)
+                Instantiate(monsterSpawnEffect, spawnPosition, Quaternion.identity);
+        }
+    }
+}
